295845375b5a4de3872036a56782518af4f4cba1
Joseph Githithu Wambura <githithudanjsef@hotmail.com>
2023-08-05 04:22:49 -0700

    ++ remove core project residual files

--- src/Clean.Architecture.Core/AdministrationModule/CompanyAggregate/Company.cs
+++
@@ -1,9 +0,0 @@
-﻿using Clean.Architecture.SharedKernel;
-using Clean.Architecture.SharedKernel.Interfaces;
-
-namespace Clean.Architecture.Core.AdministrationModule.CompanyAggregate;
-
-public class Company : EntityBase, IAggregateRoot
-{
-  public string Name { get; set; } = default!;
-}
